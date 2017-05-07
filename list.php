604fa160f6564b52a34990ba7ada03825cc3953b
Emre Timur <emretimurr@gmail.com>
2017-05-07 23:27:40 +0300

    Update list.php

--- list.php
+++ list.php
@@ -6,7 +6,7 @@
  <?php endforeach; ?>
  
  
- //Çoklu Rastgele Yazı Listeleme
+ //Çoklu Rastgele Yazı Listeleme numberposts=5 "5 yerine isteğiniz yazı adedini yazın"
  <?php $rastgele = get_posts('numberposts=5&orderby=rand'); foreach( $rastgele as $post ) :?>
     <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
             <h3><?php the_title(); ?></h3>
